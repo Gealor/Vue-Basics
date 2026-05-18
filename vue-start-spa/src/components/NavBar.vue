420f680b6d7580258dbc4d9dec7abffb6b1b6bcb
Gealor <gzazulya@gmail.com>
2026-05-19 02:50:02 +0400

    remove not used emits and little refactor in Events.js

--- vue-start-spa/src/components/NavBar.vue
+++ vue-start-spa/src/components/NavBar.vue
@@ -36,7 +36,6 @@ export default {
         NavbarLink,
     },
     props: ["activePage", "pages"],
-    emits: ['navLinkClick'],
     created() {
         this.getThemeSetting();
     },
--- vue-start-spa/src/utils/Events.js
+++ vue-start-spa/src/utils/Events.js
@@ -13,7 +13,7 @@ export default {
     },
     $emit(eventName, data) {
         if (events.has(eventName)) {
-            events.get(eventName).forEach(fn => fn(data));
+            events.get(eventName).forEach((fn) => fn(data)); // forEach т.к. у нас в качестве значения в хеш-мапе выступает список функций (обработчиков)
         }
     }
 }
